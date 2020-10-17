0b74bde0834d0935db69837cb422dccd194340db
Ivan Kayzer <ivan.kayzer@outlook.com>
2020-10-17 21:08:13 +0200

    feat: add telegram workaround to work without widget

--- src/Telegram/Provider.php
+++ src/Telegram/Provider.php
@@ -48,14 +48,47 @@ class Provider extends AbstractProvider
         return null;
     }
 
-    public function getButton()
+    public function getScript()
     {
         $botname = $this->config['bot'];
+        $botId = $this->config['client_id'];
         $callbackUrl = $this->getConfig('redirect');
 
+
         return sprintf(
-            '<script async src="https://telegram.org/js/telegram-widget.js" data-telegram-login="%s" data-size="large" data-userpic="false" data-auth-url="%s" data-request-access="write"></script>',
+            '<script src="https://telegram.org/js/telegram-widget.js" data-auth-url="%s" data-request-access="write"></script>
+            <script>
+                document.querySelector("#telegram-login").addEventListener("click", event => {
+                    window.Telegram.Login.auth(
+                        {bot_id: "%s", request_access: true},
+                        (data) => {
+                            if (!data) {
+                                console.error("authorization failed");
+                                return;
+                            }
+
+                            fetch("%s", {
+                                headers: {
+                                    "Content-Type": "application/json",
+                                    "Accept": "application/json",
+                                    "X-Requested-With": "XMLHttpRequest",
+                                    "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+                                },
+                                method: "post",
+                                credentials: "same-origin",
+                                body: JSON.stringify(data)
+                            }).then(response => {
+                                if (!response.redirected) {
+                                    return;
+                                }
+                                window.location.href = response.url;
+                            });
+                        }
+                    );
+                });
+            </script>',
             $botname,
+            $botId,
             $callbackUrl
         );
     }
@@ -65,17 +98,7 @@ class Provider extends AbstractProvider
      */
     public function redirect()
     {
-        return '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-
-        <title>Login using Telegram</title>
-    </head>
-    <body>
-        '.$this->getButton().'
-    </body>
-</html>';
+        return null;
     }
 
     /**
@@ -84,10 +107,10 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'        => $user['id'],
-            'nickname'  => $user['username'],
-            'name'      => $user['first_name'].' '.$user['last_name'],
-            'avatar'    => $user['photo_url'],
+            'id' => $user['id'],
+            'nickname' => $user['username'],
+            'name' => $user['first_name'] . ' ' . $user['last_name'],
+            'avatar' => $user['photo_url'],
         ]);
     }
 
@@ -97,19 +120,19 @@ class Provider extends AbstractProvider
     public function user()
     {
         $validator = Validator::make($this->request->all(), [
-            'id'        => 'required|numeric',
+            'id' => 'required|numeric',
             'auth_date' => 'required|date_format:U|before:1 day',
-            'hash'      => 'required|size:64',
+            'hash' => 'required|size:64',
         ]);
 
         throw_if($validator->fails(), InvalidArgumentException::class);
 
         $dataToHash = collect($this->request->except('hash'))
-                        ->transform(function ($val, $key) {
-                            return "$key=$val";
-                        })
-                        ->sort()
-                        ->join("\n");
+            ->transform(function ($val, $key) {
+                return "$key=$val";
+            })
+            ->sort()
+            ->join("\n");
 
         $hash_key = hash('sha256', $this->config['client_secret'], true);
         $hash_hmac = hash_hmac('sha256', $dataToHash, $hash_key);
